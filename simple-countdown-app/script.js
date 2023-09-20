f6ce78a754af54e0dcfb7688d6933d37cd4edbed
UrmezP <panthakiurmez@gmail.com>
2023-09-20 15:52:22 +0530

    Added close button + Fixed 0 timer bug

--- simple-countdown-app/script.js
+++ simple-countdown-app/script.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function () {
+  var counterState = 5;
+  var counterInterval;
+
   // utility methods
   const handleInput = (e) => {
     updateCounterState(e.target.value);
   };
-
-  var counterState = 5;
-  var counterInterval;
 
   const stopTimer = () => {
     stopButton.classList.add("hidden");
@@ -15,13 +15,17 @@ window.addEventListener("load", function () {
 
   const handleStart = () => {
     if (counterState == 0) {
-      this.alert("add input");
+      alert("add input");
       return;
     }
-    counterInterval = setInterval(
-      () => updateCounterState(counterState - 1),
-      1000
-    );
+
+    counterInterval = setInterval(() => {
+      updateCounterState(counterState - 1);
+      if (counterState == 0) {
+        dialog.showModal();
+        return;
+      }
+    }, 1000);
     startButton.classList.add("hidden");
     stopButton.classList.remove("hidden");
   };
@@ -35,24 +39,29 @@ window.addEventListener("load", function () {
     }
   };
 
-  // show counter
   const counter = document.getElementById("counter");
   const startButton = document.getElementById("start");
   const stopButton = document.getElementById("stop");
+
+  // open/close dialog logic
+  const dialog = document.querySelector("dialog");
+  const closeDialog = document.querySelector("dialog button");
+
+  closeDialog.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  // show counter
   counter.classList.add("fadeIn");
 
   // Start with timer set to 0
   updateCounterState(counterState);
 
   // Add listener to input field
-  this.document
+  document
     .querySelector('input[type="number"]')
     .addEventListener("input", handleInput);
 
-  this.document
-    .querySelector("button#start")
-    .addEventListener("click", handleStart);
-  this.document
-    .querySelector("button#stop")
-    .addEventListener("click", stopTimer);
+  document.querySelector("button#start").addEventListener("click", handleStart);
+  document.querySelector("button#stop").addEventListener("click", stopTimer);
 });
